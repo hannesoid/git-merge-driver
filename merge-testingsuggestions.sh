a4af27ef9fbef0459c244a16b1bcf0bca92c2701
Hannes Oud <hannes.oud.dev@gmail.com>
2019-04-30 14:51:45 +0200

    Add instructive comments in merge driver

--- merge-testingsuggestions.sh
+++ merge-testingsuggestions.sh
@@ -4,22 +4,22 @@ echo "# Merge driver called #"
 echo "#######################"
 echo ""
 
-echo "ancestor: $1"
+echo "ancestor: $1" # passed in as %O
 echo "---------"
 cat $1
 echo ""
 
-echo " current: $2"
+echo " current: $2" # passed in as %A, also expected location for the result
 echo "---------"
 cat $2
 echo ""
 
-echo "   other: $3"
+echo "   other: $3" # passed in as %B
 echo "---------"
 cat $3
 echo ""
 
-echo "Resolving conflict ..."
+echo "Resolving conflict ..." # save to %A where git expects result
 sleep 2
 echo "This is the merge result" > $2
 echo "Conflict resolved!"
