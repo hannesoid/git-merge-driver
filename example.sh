70dd45397ca74abeee511de675a3f6cadc343f5a
Hannes Oud <hannes.oud.dev@gmail.com>
2019-04-30 14:47:46 +0200

    Modify naming for testingsuggestions

--- example.sh
+++ example.sh
@@ -1,30 +1,30 @@
 # This script will run a quick demo of the merge driver.
-# It will cause a merge conflict in a 'my-file.mrg' file.
+# It will cause a merge conflict in a 'TESTINGSUGGESTIONS.md' file.
 # -------------------------------------------------------
 
 # Run the mergetool-setup.sh script to configure the merge driver
 ./mergetool-setup.sh
 
-# Add the my-merge-tool.sh to PATH
+# Add the merge-testing-suggestions.sh to PATH
 PATH=$PATH:`pwd`
 
 # Clean up any previous example runs
-git checkout master
+git checkout midnode-merger
 git branch -D demo-branch-1
 git branch -D demo-branch-2
 
-# Create 'my-file.mrg' on branch 1
+# Create 'TESTINGSUGGESTIONS.md' on branch 1
 git checkout -b demo-branch-1
-echo "created on: demo-branch-1" > my-file.mrg
-git add my-file.mrg
-git commit -m"demo-branch-1: added my-file.mrg"
+echo "\n### created on: demo-branch-1\n" > TESTINGSUGGESTIONS.md
+git add TESTINGSUGGESTIONS.md
+git commit -m "demo-branch-1: added TESTINGSUGGESTIONS.md"
 
-# Create 'my-file.mrg' on branch 2
+# Create 'TESTINGSUGGESTIONS.md' on branch 2
 git checkout master
 git checkout -b demo-branch-2
-echo "created on: demo-branch-2" > my-file.mrg
-git add my-file.mrg
-git commit -m"demo-branch-2: added my-file.mrg"
+echo "\ncreated on: demo-branch-2\n" > TESTINGSUGGESTIONS.md
+git add TESTINGSUGGESTIONS.md
+git commit -m "demo-branch-2: added TESTINGSUGGESTIONS.md"
 
 # Merge the two branches, causing a conflict
-git merge -m"Merged in demo-branch-1" demo-branch-1
+git merge -m "Merged in demo-branch-1" demo-branch-1
